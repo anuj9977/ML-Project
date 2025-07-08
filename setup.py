b3d54904b307f781db70c82c9bf1946bcad90159
Anuj <anujpatel997780@gmail.com>
2025-07-08 14:50:57 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='ML-project',
+    version='0.0.1',
+    author='Anuj Patel',
+    author_email='anujpatel997780@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+    
+)
